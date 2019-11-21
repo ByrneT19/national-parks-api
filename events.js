ff81dcebfdf6f9a570fbb57a4570679e7f6bb692
Tam Byrne <mercadier122@gmail.com>
2019-11-21 15:06:27 +0000

    all

---
+++ events.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const apiKey = '8hXcxO2cfZCudAX4QJpcfoEy6Ikfu8xweIUImtu0'
+const apiUrl = 'https://developer.nps.gov/api/v1'
+const stC = $('#js-state').val();
+
+function findPark() {
+    fetch(`${apiUrl}/${stC}&${apiKey}`)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(JSON.stringify(responseJson)))
+    .catch(err => {
+        $('#js-error-message').html(`Sorry something went wrong! Please try again!`)
+    })        
+}
+
+function queryParams(params) {
+    const query = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return query.join('&');
+}
+
+function getPark(maxResults=10) {
+    const params = {
+        key: apiKey,
+        stateCode: stC,
+        maxResults
+    }
+}
+
+function runApi() {
+    findPark();
+}
+
+$(runApi);
+
+  
