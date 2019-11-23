afd8b3784bac700262e17a495f6fce0d6f4669e3
Tam Byrne <mercadier122@gmail.com>
2019-11-23 23:41:48 +0000

    js

--- events.js
+++ events.js
@@ -2,14 +2,17 @@
 
 const apiKey = '8hXcxO2cfZCudAX4QJpcfoEy6Ikfu8xweIUImtu0'
 const apiUrl = 'https://developer.nps.gov/api/v1/parks?stateCode='
-const stC = $('#js-state').val()//.toUpperCase().split(',');
+const stC = $('#js-state').val();
+const maxResults = $('#js-max-results').val();
 
 function findPark(stC, maxResults) {
     fetch(`${apiUrl}${stC}&limit=${maxResults}&api_key=${apiKey}`)
     .then(response => {
+        // debugger;
         if (!response.ok) {
             throw new Error(response.statusText)
-        } return response.json();
+        } 
+        return response.json();
     })
     .then(responseJson => showPark(responseJson))
     .catch(err => {
@@ -32,23 +35,32 @@ function getPark(query, maxResults=10) {
     queryParams(params);
 }*/
 
+/*function emptyInput() {
+    $("#js-state").val("");
+    $("#js-max-results").val("");
+  }*/
+
 function showPark(responseJson) {
-    $('#js-state').empty();
-    $('#js-max-results').empty();
-    let i = responseJson;
-    for(i = 0; responseJson.data.length; i++) {
+    $('#js-state').val("");
+    $('#js-max-results').val("");
+    $('#park-list').empty();
+    let i = null;
+    // debugger;
+    for(i = 0; i < responseJson.data.length; i++) {
+        // debugger;
         $('#park-list').append(
         `<li class=actualResults>
         <h3><a href="${responseJson.data[i].url == null ? '' : responseJson.data[i].url}" target="_blank">${responseJson.data[i].fullName}</a></h3>
         <p>States: ${responseJson.data[i].states}</p>
         <p class="details">${responseJson.data[i].description}</p>
         <p class="details">${responseJson.data[i].weatherInfo}</p>
+        
         </li>`
         )
     }
 }
 
-function watchForm() {
+/*function watchForm() {
     $('#search').on('submit', function(e) {
       e.preventDefault();
       console.log(e);
@@ -57,11 +69,26 @@ function watchForm() {
     //   getPark(maxResults);
        findPark(stC, maxResults);
     })
-  }
+  }*/
+
+function watchForm() {
+    $('#search').on('submit', function(e) {
+    e.preventDefault();
+    console.log(e);
+      const stC = $('#js-state').val().toUpperCase().split(',');
+      const maxResults = $('#js-max-results').val();
+    //   getPark(maxResults);
+        if(stC && maxResults){
+            findPark(stC, maxResults);
+            } else {
+                alert("error in form")
+            }
+        })
+        // $('#park-list').empty();
+}
 
 function runApi() {
     watchForm();
-    // getPark(maxResults)
 }
 
 $(runApi);
